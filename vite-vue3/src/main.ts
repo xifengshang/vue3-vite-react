9529e33b5b64a926b21ad9e57b220ba9bb09677a
hanrui <18862245449@163.com>
2023-07-30 20:32:53 +0800

    add pinia

--- vite-vue3/src/main.ts
+++ vite-vue3/src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import router from './router'
+import pinia from './store'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
